841dee4ec0dac9f5ca5e2b9f1264de75a1db3033
jacharje <jdp_jan@outlook.com>
2025-04-27 22:33:17 +0530

    Fix connection

--- queue/emailQueue.js
+++ queue/emailQueue.js
@@ -1,11 +1,13 @@
 const { Queue } = require('bullmq');
 const Redis = require('ioredis');
 
-const connection = new Redis(process.env.REDIS_URL, {
+const redisConnection = new Redis(process.env.REDIS_URL, {
     tls: { rejectUnauthorized: false },
     maxRetriesPerRequest: null,
 });
-
-const emailQueue = new Queue('emailQueue', { connection });
+  
+const emailQueue = new Queue('emailQueue', {
+connection: redisConnection,
+});
 
 module.exports = emailQueue;
